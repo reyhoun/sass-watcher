94aade17599052ea7e8b411b53d27fa93a79e983
Parhum Khoshbakht <parhum.kh@gmail.com>
2015-01-21 07:55:09 +0330

    Update scssphp library to v0.1.1

--- sass-watcher.php
+++ sass-watcher.php
@@ -3,7 +3,7 @@
  * Plugin Name: Sass Watcher
  * Plugin URI: https://github.com/reyhoun/sass-watcher
  * Description: A PHP Sass compiler that compatible with ACF 5 plugin and use SCSSPHP to compile Sass Codes
- * Version: 1.5.0
+ * Version: 1.5.1
  * Author: Reyhoun
  * Author URI: http://reyhoun.com/
  * License: GPLv2 or later
